7841fa2413036ca9e5de7f0cce600d0070a3e364
ahpraveen <aha.praveen@gmail.com>
2023-06-07 08:40:46 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,66 @@
+from typing import List
+from fastapi import FastAPI, HTTPException
+from uuid import UUID
+from models import Student, Gender, Role, StudentUpdateRequest
+
+app = FastAPI()
+
+db: List[Student] = [
+    Student(
+         id=UUID("6eea7f3f-3fdd-42ea-a4b0-c75e56b3ccae"), 
+         first_name="Jamil", 
+         last_name="Ahmed",
+         gender=Gender.female,
+         roles=[Role.student]
+    ),
+    Student(
+         id=UUID("92065400-dbf6-4eb0-9f9b-6e8db5a332d6"), 
+         first_name="Alex", 
+         last_name="Jones",
+         gender=Gender.male,
+         roles=[Role.admin, Role.user]
+    )
+]
+
+@app.get("/")
+async def root():
+    return{"hello": "world"}
+
+@app.get("/api/v1/users")
+async def fetch_users():
+    return db;
+
+@app.post("/api/v1/users")
+async def register_user(user: Student):
+    db.append(user)
+    return{"id":user.id}
+
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+   for user in db:
+       if user.id == user_id:
+           db.remove(user)
+           return
+   raise HTTPException(
+            status_code=404,
+            detail=f"user with id: {user_id} does not exists"
+        )
+
+@app.put("/api/v1/users/{user_id}")
+async def update_user(user_update: StudentUpdateRequest, user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            if user_update.first_name is not None:
+                user.first_name = user_update.first_name
+            if user_update.last_name is not None:
+                user.last_name = user_update.last_name
+            if user_update.middle_name is not None:
+                user.middle_name = user_update.middle_name
+            if user_update.roles is not None:
+                user.roles = user_update.roles
+            return
+    raise HTTPException(
+            status_code=404,
+            detail=f"user with id: {user_id} does not exists"
+        )
+
